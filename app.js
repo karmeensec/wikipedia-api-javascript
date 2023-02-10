3f17ffe3567e3052e6162c0373523231ab7ec429
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-11 00:01:20 +0100

    selected elements - DOM

---
+++ app.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector('#submit');
+const input = document.querySelector('#input');
+const error = document.querySelector('#error');
+const results = document.querySelector('#results');
