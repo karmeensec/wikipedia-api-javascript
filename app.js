f9ed59f0a781642e0d3dd745172480aa5bfb2994
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-11 00:23:00 +0100

    input, btn disblae/enable functions

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ const btn = document.querySelector('#submit');
 const input = document.querySelector('#input');
 const error = document.querySelector('#error');
 const results = document.querySelector('#results');
+
+const disableUI = () => {
+
+    btn.disabled = true;
+    input.disabled = true;
+
+}
+
+const enableUI = () => {
+
+    btn.disabled = false;
+    input.disabled = false;
+
+}
