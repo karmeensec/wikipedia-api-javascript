67f08c09818eb78fafbffe7c88d3b6d24b5c781f
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-11 00:27:21 +0100

    helper functions completed

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const input = document.querySelector('#input');
 const error = document.querySelector('#error');
 const results = document.querySelector('#results');
 
+
+// Helpers
+
 const disableUI = () => {
 
     btn.disabled = true;
@@ -14,5 +17,18 @@ const enableUI = () => {
 
     btn.disabled = false;
     input.disabled = false;
+
+}
+
+const clearPrevResults = () => {
+
+    results.innerHTML = '';
+
+}
+
+const checkEmptyInput = (input) => {
+
+    if (!input || input === '') return true;
+    else return false;
 
 }
