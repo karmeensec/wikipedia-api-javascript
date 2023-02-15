829c42adfcd3d7ca3d8c2568dfd1054e3d76b5bd
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-16 00:50:20 +0400

    showing the results

--- app.js
+++ app.js
@@ -57,6 +57,45 @@ const showError = (error) => {
 
 
 
+const showResults = function(results) {
+
+    results.forEach( (result) => {
+
+        results.innerHTML += `
+        
+            <div id="results" class="results__item">
+            
+                <a href="https://en.wikipedia.org/?curid=${result.pageId}" target="_blank" class="card animated bounceInUp">
+                    <h2 class="results__item__title">${result.title}</h2>
+                    <p class="results__item__intro">${result.intro}</p>
+                </a>    
+
+            </div>
+
+        `
+
+    } )
+
+};
+
+
+const gatherData = (pages) => {
+
+    const results =  Object.values(pages).map( (page) => ({
+
+        pageId: page.pageId,
+        title: page.title,
+        intro: page.extract,
+
+    }));
+
+    showResults(results);
+
+}
+
+
+
+
 const getData = async function() {
 
    const userInput = input.value;
@@ -70,13 +109,21 @@ const getData = async function() {
 
     const {data} =  await axios.get(endpoint, {params});
 
-   } catch (err){
-    showError(err);
+    if (data.error) throw new Error(data.error.info);
+
+    gatherData(data.query.pages);
+
+   } catch (error){
+    showError(error);
    } finally {
     enableUI();
    }
 
 }
+
+
+
+
 
 const keyEvent = function(e) {
 
